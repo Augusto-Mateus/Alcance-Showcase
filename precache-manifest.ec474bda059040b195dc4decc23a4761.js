28494e6c513879a7063776ab075ed6a0c1cb22e5
Mateus Duarte <m.asuheth@gmail.com>
2019-02-19 15:56:13 -0300

    Updates

---
+++ precache-manifest.ec474bda059040b195dc4decc23a4761.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e8a3fd3c4af8f00140dde806bfa4890",
+    "url": "/Alcance-Showcase/static/media/DuraçãoDeCampanha.0e8a3fd3.png"
+  },
+  {
+    "revision": "286c88c940901d4427d5",
+    "url": "/Alcance-Showcase/static/css/main.69556776.chunk.css"
+  },
+  {
+    "revision": "d9fe0fac8d35d85a29ce",
+    "url": "/Alcance-Showcase/static/js/runtime~main.d9fe0fac.js"
+  },
+  {
+    "revision": "28f6aedb1f5d93ff2786",
+    "url": "/Alcance-Showcase/static/js/2.28f6aedb.chunk.js"
+  },
+  {
+    "revision": "696231b7e2dc3256ebf50fee3d2324c4",
+    "url": "/Alcance-Showcase/static/media/LandingPageBackground.696231b7.png"
+  },
+  {
+    "revision": "2e7182506e9f24acf22ae0e35ab3b075",
+    "url": "/Alcance-Showcase/static/media/Logo.2e718250.png"
+  },
+  {
+    "revision": "eb1219207fac97f814861ac3d2185636",
+    "url": "/Alcance-Showcase/static/media/CONTAS.eb121920.png"
+  },
+  {
+    "revision": "286c88c940901d4427d5",
+    "url": "/Alcance-Showcase/static/js/main.286c88c9.chunk.js"
+  },
+  {
+    "revision": "09890bd9bf6b32883173e007211b2589",
+    "url": "/Alcance-Showcase/static/media/calendario.09890bd9.png"
+  },
+  {
+    "revision": "bce821312346987ca61839a14bc950a5",
+    "url": "/Alcance-Showcase/static/media/PAGAMENTO.bce82131.png"
+  },
+  {
+    "revision": "d1f0660b366f76b1e1818df9f5295c2e",
+    "url": "/Alcance-Showcase/static/media/Paypal.d1f0660b.png"
+  },
+  {
+    "revision": "3e974b9f1c7863c3270160c0ca211098",
+    "url": "/Alcance-Showcase/static/media/públicoAlvo.3e974b9f.png"
+  },
+  {
+    "revision": "9de4a0576238a6f524b9bb3c4b4b7eb4",
+    "url": "/Alcance-Showcase/static/media/segmentação.9de4a057.png"
+  },
+  {
+    "revision": "e1065d09ab330ba238abcb29975f2d7f",
+    "url": "/Alcance-Showcase/index.html"
+  }
+];
